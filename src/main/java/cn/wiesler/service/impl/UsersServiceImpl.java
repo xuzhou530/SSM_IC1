4e37bd6e6548903d1187976732fd4d1bab477f10
时光机器 <ethan2012@outlook.com>
2016-07-11 20:32:21 +0800

    提交在有可拓展的pojo类时，事务的处理和传值方式。

--- src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
+++ src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
@@ -1,13 +1,30 @@
 package cn.wiesler.service.impl;
 
+import cn.wiesler.dao.UsersMapper;
 import cn.wiesler.pojo.Users;
+import cn.wiesler.pojo.UsersCustom;
 import cn.wiesler.service.IUsersService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by 时光机器 on 2016/7/11.
  */
+@Service("usersService")
 public class UsersServiceImpl implements IUsersService {
-    public Users getUsersById(int uid) {
-        return null;
+    @Resource
+    private UsersMapper usersMapper;
+
+    public UsersCustom getUsersById(int uid) {
+        Users users = usersMapper.selectByPrimaryKey(uid);
+        //中间进行业务处理....
+        //....
+        //返回UsersCustom
+        UsersCustom usersCustom = new UsersCustom();
+        //将users中的属性值拷贝到usersCustom中，再返回
+        BeanUtils.copyProperties(users, usersCustom);
+        return usersCustom;
     }
 }
