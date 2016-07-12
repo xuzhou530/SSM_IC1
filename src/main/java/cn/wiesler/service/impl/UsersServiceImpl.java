e060ae5624d1e724195dee740dab6a5fb2e5a74f
时光机器 <ethan2012@outlook.com>
2016-07-12 15:39:07 +0800

    重写getUserById方法，保证userCustom在查不到的情况下是空值返回到Controller的方法中的，保证Controller中方法可以通过验证userCustom是否为空而使用自定义异常

--- src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
+++ src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
@@ -17,14 +17,33 @@ public class UsersServiceImpl implements IUsersService {
     @Resource
     private UsersMapper usersMapper;
 
-    public UsersCustom getUsersById(int uid) {
+    public UsersCustom getUsersById(Integer uid) {
         Users users = usersMapper.selectByPrimaryKey(uid);
         //中间进行业务处理....
         //....
-        //返回UsersCustom
-        UsersCustom usersCustom = new UsersCustom();
+        //这里定义userCustom，下面可以返回UsersCustom
+        //如果在下面的if外层新建UserCustom构造器的话，userCustom不为空，Controller层就不能通过判断返回的结果是否为空
+        //而进行自定义异常处理
+        UsersCustom usersCustom = null;
         //将users中的属性值拷贝到usersCustom中，再返回
-        BeanUtils.copyProperties(users, usersCustom);
+        //当users内容按ID查找不为空时，再进行属性值拷贝，是为了Controller中showUsers方法进行自定义异常处理
+        if(users != null){
+            usersCustom = new UsersCustom();
+            BeanUtils.copyProperties(users, usersCustom);
+        }
         return usersCustom;
     }
+
+    public void addUsers(UsersCustom usersCustom) {
+        usersMapper.insert(usersCustom);
+    }
+
+/*
+    public void updateUsersById(Integer uid, UsersCustom usersCustom) {
+        //在业务层插入、更新数据时要进行是否为空等校验,int不能校验，所以一般都定义为Integer
+        usersCustom.setUid(uid);
+        usersMapper.updateByPrimaryKey(usersCustom);
+    }
+*/
+
 }
