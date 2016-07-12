97e9beedc2ff3d9db8f1ab9227301f4699be0898
时光机器 <ethan2012@outlook.com>
2016-07-12 15:45:40 +0800

    重写getUserById方法，抛出异常，一般与业务有关的异常在Service层中抛出

--- src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
+++ src/main/java/cn/wiesler/service/impl/UsersServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.wiesler.service.impl;
 
 import cn.wiesler.dao.UsersMapper;
+import cn.wiesler.exception.CustomException;
 import cn.wiesler.pojo.Users;
 import cn.wiesler.pojo.UsersCustom;
 import cn.wiesler.service.IUsersService;
@@ -17,16 +18,17 @@ public class UsersServiceImpl implements IUsersService {
     @Resource
     private UsersMapper usersMapper;
 
-    public UsersCustom getUsersById(Integer uid) {
+    public UsersCustom getUsersById(Integer uid) throws Exception{
         Users users = usersMapper.selectByPrimaryKey(uid);
+        //一般与业务相关的异常都在Service层中抛出
+        if(users == null){
+            throw new CustomException("查找的用户不存在！");
+        }
         //中间进行业务处理....
         //....
         //这里定义userCustom，下面可以返回UsersCustom
-        //如果在下面的if外层新建UserCustom构造器的话，userCustom不为空，Controller层就不能通过判断返回的结果是否为空
-        //而进行自定义异常处理
         UsersCustom usersCustom = null;
         //将users中的属性值拷贝到usersCustom中，再返回
-        //当users内容按ID查找不为空时，再进行属性值拷贝，是为了Controller中showUsers方法进行自定义异常处理
         if(users != null){
             usersCustom = new UsersCustom();
             BeanUtils.copyProperties(users, usersCustom);
