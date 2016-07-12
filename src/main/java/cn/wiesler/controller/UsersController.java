bd82bbf6ade76fad1a89c7e0c3fa714d4a327c1a
时光机器 <ethan2012@outlook.com>
2016-07-12 16:27:36 +0800

    一般与业务无关的异常在Controller中抛出 利用@PathVariable添加了RESTFul风格的URL

--- src/main/java/cn/wiesler/controller/UsersController.java
+++ src/main/java/cn/wiesler/controller/UsersController.java
@@ -7,6 +7,7 @@ import cn.wiesler.service.IUsersService;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,8 +22,9 @@ public class UsersController {
     @Resource
     private IUsersService usersService;
     //浏览器地址栏里的目录是根据@RequestMapping来写的，和return的真实目录是没有关系的
-    @RequestMapping("/showUsers")
-    public String showUsers(Model model, @RequestParam(value="uid", defaultValue = "1") Integer uid) throws Exception{
+    //去掉了@RequestParam因为方法写的有问题，不好控制RESTFul，RESTFul中{uid}就是需要传入的参数
+    @RequestMapping("/showUsers/{uid}")
+    public String showUsers(Model model, @PathVariable("uid") Integer uid) throws Exception{
         UsersCustom usersCustom = this.usersService.getUsersById(uid);
         //判断根据id查找的数据是否存在。一般与业务无关的异常在Controller中抛出，例如输入长度等
         /*if(usersCustom == null){
