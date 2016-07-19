703df84c6a1536e25f3acf0b0082d9b22644b9e0
时光机器 <ethan2012@outlook.com>
2016-07-19 15:08:27 +0800

    multi table query, because of the relationship of Users and Orders is one-to-multi (a user contains multiple orders), make a object orderCustomList in here.

--- src/main/java/cn/wiesler/pojo/UsersCustom.java
+++ src/main/java/cn/wiesler/pojo/UsersCustom.java
@@ -1,7 +1,21 @@
 package cn.wiesler.pojo;
 
+import java.util.List;
+
 /**
  * Created by 时光机器 on 2016/7/11.
  */
 public class UsersCustom extends Users {
+
+    //Mapping properties, Query Users associated with Orders and Orderdetails and Menu (multi-to-multi query)
+    //The corresponding relationship between the Users and the Orders is one-to-multi, so make a List to here
+    List<OrdersCustom> ordersCustomList;
+
+    public List<OrdersCustom> getOrdersCustomList() {
+        return ordersCustomList;
+    }
+
+    public void setOrdersCustomList(List<OrdersCustom> ordersCustomList) {
+        this.ordersCustomList = ordersCustomList;
+    }
 }
